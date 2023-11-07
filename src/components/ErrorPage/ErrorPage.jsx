d77d9810a52c2b9e122ddb3506dd24905bb2f9d9
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-08 03:26:49 +0600

    complete the react router

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,16 @@
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+
+    const error = useRouteError()
+    console.log(error)
     return (
-        <div className="text-center">
+        <div className="text-center mt-44">
+            <p className="text-7xl">{error.status}</p>
             <h1 className="font-semibold text-3xl">Opps!</h1>
-            <p>Nothing Here</p>            
+            <p>Nothing Here</p>
+            <p>{error.data}</p>
+            <Link to="/"><button className="btn border-2 p-2 rounded-lg bg-gray-900 text-white">Home</button></Link>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
@@ -6,11 +6,11 @@ const Header = () => {
         <div className="text-center">
             <h3 className="text-3xl">React Router</h3>
             <nav className="space-x-3 font-bold text-xl mt-2">
-                <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/about">About</Link>
+                <NavLink className={({isActive,isPending})=> isActive ? 'text-blue-600' : isPending ? 'text-black' : '' }  to="/">Home</NavLink>
+                <NavLink className={({ isActive, isPending }) => isActive ? 'text-blue-600' : isPending ? 'text-black' : ''} to="/users">Users</NavLink>
+                <NavLink className={({isActive,isPending})=> isActive ? 'text-blue-600' : isPending ? 'text-black' : '' }  to="/posts">Posts</NavLink>
+                <NavLink className={({isActive,isPending})=> isActive ? 'text-blue-600' : isPending ? 'text-black' : '' }  to="/contact">Contact</NavLink>
+                <NavLink className={({isActive,isPending})=> isActive ? 'text-blue-600' : isPending ? 'text-black' : '' }  to="/about">About</NavLink>
             </nav>
         </div>
     );
